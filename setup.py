0dd53d2c0e7ed7297f715d295851720acbed6111
gokhanKaradas <gokhan.karadas1992@gmail.com>
2015-03-20 23:29:31 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+__author__ = 'gokhan'
+
+from setuptools import setup
+
+setup(
+    name='PythonPostcodesWrapper',
+    version='1.0.0',
+    author='Gokhan Karadas',
+    author_email='gokhan.karadas1992@gmail.com',
+    packages=[],
+    scripts=['.py'],
+    url='https://github.com/previousdeveloper/PythonPostcodesWrapper',
+    license='LICENSE',
+    description='Postcode & Geolocation API for UK',
+    install_requires=['requests'],
+    include_package_data=True
+)
+
