c8005165923a0d38362ac8102de758c5dc1149b6
gokhanKaradas <gokhan.karadas1992@gmail.com>
2015-03-21 13:19:15 +0200

    added some basic method.

--- postcodes/client.py
+++ postcodes/client.py
@@ -3,9 +3,31 @@ import requests
 
 
 class PostCode(object):
-    def getRandomPostCodes(url):
-        data = requests.get(url).text
+    def getLookupPostCode(self, postcode):
+        data = requests.get("http://api.postcodes.io/postcodes/" + postcode).text
         return data
 
 
-    print  getRandomPostCodes("http://api.postcodes.io/random/postcodes")
+    def getLookupPostcodes(payload):
+        data = requests.post("http://api.postcodes.io/postcodes", payload).text
+        return data
+
+
+    def getNearestPostcodes(lon, lang):
+        data = requests.get("http://api.postcodes.io/postcodes?lon=" + lon + "&lat=" + lang).text
+        return data
+
+
+    def getBulkReverseGecoding(payload):
+        data = requests.post("http://api.postcodes.io/postcodes", payload).text
+        return data
+
+
+    @staticmethod
+    def getRandomPostCodes():
+        pass
+        data = requests.get("http://api.postcodes.io/random/postcodes").text
+        return data
+
+
+print PostCode.getRandomPostCodes()
